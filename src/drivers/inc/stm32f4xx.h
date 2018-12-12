6d58ecdcac21830152513d0a5a72f4870b19ad2a
Dani White <white.dani.m@gmail.com>
2018-12-12 14:45:24 -0500

    Some hacks to make it compile for now

--- src/drivers/inc/stm32f4xx.h
+++ src/drivers/inc/stm32f4xx.h
@@ -1,9 +1,15 @@
-#include "can_raw.h"
+#ifndef __STM32F4XX_CAN_H
+#define __STM32F4XX_CAN_H
 
-#include "stm32f4xx_hal_can.h"
+#include "can_raw.h"
+#include "stm32f4xx_hal.h"
+
 #include <stdint.h>
 
 typedef HAL_StatusTypeDef CANlib_Transmit_Error_T;
+typedef HAL_StatusTypeDef CANlib_Init_Error_T;
 
 HAL_StatusTypeDef CANlib_Init(uint32_t baudrate);
 HAL_StatusTypeDef CANlib_TransmitFrame(Frame *frame);
+
+#endif // __STM32F4XX_CAN_H
--- src/drivers/src/stm32f4xx.c
+++ src/drivers/src/stm32f4xx.c
@@ -1,7 +1,9 @@
 #include "can_raw.h"
 
-#include "stm34f2xx_hal_can.h"
+#include "stm32f4xx_hal.h"
 #include <stdint.h>
+
+#define USING_CAN1
 
 static CAN_HandleTypeDef can1;
 static CAN_HandleTypeDef can2;
@@ -41,25 +43,23 @@ HAL_StatusTypeDef CANlib_Init(uint32_t baudrate) {
   #if defined(USING_CAN1) || defined(USING_CAN2)
   // We need to initialize CAN1 regardless of wheter we are using CAN1 or CAN2
   can1.Instance = CAN1;
-  config_handle_parameters(&can1)
-  CHECK_HAL_OK(HAL_CAN_Init(&can1));
-  CHECK_HAL_OK(HAL_CAN_ConfigFilter(&can1, &filter));
+  config_handle_parameters(&can1);
+  HAL_CAN_Init(&can1);
+  HAL_CAN_ConfigFilter(&can1, &filter);
   HAL_CAN_Start(&can1);
   #endif
   #ifdef USING_CAN2
   can2.Instance = CAN2;
   config_handle_parameters(&can2);
-  CHECK_HAL_OK(HAL_CAN_Init(&can2));
-  CHECK_HAL_OK(HAL_CAN_ConfigFilter(&can2, &filter));
+  HAL_CAN_Init(&can2);
+  HAL_CAN_ConfigFilter(&can2, &filter);
   HAL_CAN_Start(&can2);
   #elif defined(USING_CAN3)
   can3.Instance = CAN3;
   config_handle_parameters(&can3);
-  CHECK_HAL_OK(HAL_CAN_Init(&can3));
-  CHECK_HAL_OK(HAL_CAN_ConfigFilter(&can3, &filter));
+  HAL_CAN_Init(&can3);
+  HAL_CAN_ConfigFilter(&can3, &filter);
   HAL_CAN_Start(&can3);
-  #else
-  #error Can library included, but none of USING_CAN1, USING_CAN2, or USING_CAN3 are defined
   #endif
 }
 
@@ -69,14 +69,14 @@ HAL_StatusTypeDef CANlib_TransmitFrame(Frame *frame) {
   hcan = &can1;
   #elif defined(USING_CAN2)
   hcan = &can2;
-  #elif defind(USING_CAN3)
+  #elif defined(USING_CAN3)
   hcan = &can3;
   #endif
 
   CAN_TxHeaderTypeDef pHeader;
   uint32_t pTxMailbox = 0;
 
-  pHeader.DLC = frame->len;
+  pHeader.DLC = frame->dlc;
   pHeader.StdId = frame->id;
   pHeader.IDE = frame->extended ? CAN_ID_EXT: CAN_ID_STD;
   pHeader.RTR = CAN_RTR_DATA; // Data frame (as opposed to a remote frame)
