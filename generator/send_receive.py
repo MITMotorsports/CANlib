e1173f9f60a20cea4d8cfd9a48fa0a0fe8293ea1
Dani White <white.dani.m@gmail.com>
2019-01-18 09:53:06 -0500

    Switch send_receive to use frame_handler

--- generator/send_receive.py
+++ generator/send_receive.py
@@ -6,7 +6,7 @@ constants.h or main.py to write all files.
 import sys
 sys.path.append("ParseCAN")
 import ParseCAN
-from common import send_recieve_path, coord, templ, ifndef, endif, is_multplxd, is_multplxd
+from common import send_recieve_path, coord, templ, ifndef, endif, frame_handler
 
 
 def write(can, output_path=send_recieve_path):
@@ -22,49 +22,37 @@ def write(can, output_path=send_recieve_path):
         fw('#include "pack_unpack.h"\n\n')
 
         def define_pub_frame(frame, name_prepends):
-            if is_multplxd(frame):
-                for sub_frame in frame.frame:
-                    define_pub_frame(sub_frame, name_prepends + '_' + frame.name)
-            else:
-                tot_name = coord(name_prepends, frame.name,
-                    prefix=False)
-                fw('void send_{}_msg(CANlib_{}_T *inp)'.format(
-                    tot_name, tot_name) + ' {\n'
-                )
+            tot_name = coord(name_prepends, frame.name,
+                prefix=False)
+            fw('void send_{}_msg(CANlib_{}_T *inp)'.format(
+                tot_name, tot_name) + ' {\n'
+            )
 
-                if frame.period is not None:
-                    fw('\tLIMIT(CANlib_{});\n'.format(tot_name))
+            if frame.period is not None:
+                fw('\tLIMIT(CANlib_{});\n'.format(tot_name))
 
-                fw('\tCANlib_Transmit_{}(inp);\n'.format(tot_name, tot_name) + '}\n\n')
+            fw('\tCANlib_Transmit_{}(inp);\n'.format(tot_name, tot_name) + '}\n\n')
 
         def define_sub_frame(frame, name_prepends):
-            if is_multplxd(frame):
-                for sub_frame in frame.frame:
-                    define_sub_frame(sub_frame, name_prepends + '_' + frame.name)
-            else:
-                tot_name = coord(name_prepends, frame.name,
-                    prefix=False)
-                fw('void handle_{}_msg(Frame *frame)'.format(
-                   tot_name, tot_name) + ' {\n' + '\tCANlib_Unpack_{}(frame, &{}_inp);\n'.format(tot_name, tot_name) + '}\n\n')
+            tot_name = coord(name_prepends, frame.name,
+                prefix=False)
+            fw('void handle_{}_msg(Frame *frame)'.format(
+                tot_name, tot_name) + ' {\n' + '\tCANlib_Unpack_{}(frame, &{}_inp);\n'.format(tot_name, tot_name) + '}\n\n')
         
         def declare_struct(frame, name_prepends):
-                if is_multplxd(frame):
-                    for sub_frame in frame.frame:
-                        declare_struct(sub_frame, name_prepends + '_' + frame.name)
-                else:
-                    tot_name = coord(name_prepends, frame.name, prefix=False)
-                    fw('CANlib_{}_T {}_inp;\n'.format(
-                        tot_name, tot_name))
+            tot_name = coord(name_prepends, frame.name, prefix=False)
+            fw('CANlib_{}_T {}_inp;\n'.format(
+                tot_name, tot_name))
 
         for bus in can.bus:
             for msg in bus.frame:
-                declare_struct(msg, bus.name)
+                frame_handler(msg, bus.name, declare_struct)
         fw('\n')
 
         for bus in can.bus:
             for msg in bus.frame:
-                define_pub_frame(msg, bus.name)
-                define_sub_frame(msg, bus.name)
+                frame_handler(msg, bus.name, define_pub_frame)
+                frame_handler(msg, bus.name, define_sub_frame)
         fw('\n')
 
 
