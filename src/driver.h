ccf22dbda571e8042a250cfb25a98867c43ab67d
Dani White <white.dani.m@gmail.com>
2019-01-22 17:43:13 -0500

    Add include guards to driver.h

--- src/driver.h
+++ src/driver.h
@@ -1,6 +1,11 @@
+#ifndef __DRIVER_H
+#define __DRIVER_H
+
 #include "drivers/inc/stm32f4xx.h"
 #include "bus.h"
 
 CANlib_Transmit_Error_T CANlib_TransmitFrame(Frame *frame, CANlib_Bus_T bus);
 void CANlib_ReadFrame(Frame *frame, CANlib_Bus_T bus);
 CAN_Raw_Bus_T CANlib_GetRawBus(CANlib_Bus_T bus);
+
+#endif // __DRIVER_H
