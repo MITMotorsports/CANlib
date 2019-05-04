616a1f58cfcca25b48dfd868d53819854db4988f
Mingfei <mingfeid@mit.edu>
2019-05-04 15:23:44 -0400

    HandleFrame returns bool

--- generator/computers_c.py
+++ generator/computers_c.py
@@ -64,6 +64,7 @@ def write(can, computers, output_path=computer_c_dir_path):
         with open(f_path, 'w') as f:
             fw = f.write
             fw('#include <time.h>\n')
+            fw('#include <stdbool.h>\n')
             fw('#include "pack_unpack.h"\n')
             fw('#include "canlib_{}.h"\n\n'.format(computer.name))
 
@@ -128,7 +129,7 @@ def write(can, computers, output_path=computer_c_dir_path):
                 fw('\tCANlib_update_can_{}();\n'.format(busnm))
             fw('}\n\n')
 
-            fw('void CANlib_HandleFrame(TimestampedFrame *ts_frame, time_t stamp, CAN_Raw_Bus_T raw_bus) {\n')
+            fw('bool CANlib_HandleFrame(TimestampedFrame *ts_frame, time_t stamp, CAN_Raw_Bus_T raw_bus) {\n')
             if len(computer.participation['name']['can'].subscribe.keys()) > 0: # check if computer receives messages
                 fw('\tif (CANlib_ReadFrame(&(ts_frame->frame), raw_bus)) {\n')
                 fw('\t\tts_frame->stamp = stamp;\n')
@@ -139,11 +140,15 @@ def write(can, computers, output_path=computer_c_dir_path):
                         '\t\t\t\tCANlib_HandleFrame_{}(ts_frame);\n'.format(bus) +
                         '\t\t\t\tbreak;\n'
                     )
-                fw('\t\t\tdefault:\n\t\t\tbreak;\n')
+                fw('\t\t\tdefault:\n')
+                fw('\t\t\t\tbreak;\n')
                 fw('\t\t}\n')
+                fw('\t\treturn true;\n')
                 fw('\t}\n')
+                fw('\treturn false;\n')
             else: # prevent unused warning
                 fw('\tUNUSED(ts_frame);\n')
                 fw('\tUNUSED(stamp);\n')
                 fw('\tUNUSED(raw_bus);\n')
+                fw('\treturn false;\n')
             fw('}\n')
