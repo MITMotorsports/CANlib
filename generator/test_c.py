7121471025562cbe3ee10f2078619c5c5c57875c
Dani White <white.dani.m@gmail.com>
2019-06-06 20:55:27 -0400

    Fix test_c.py

--- generator/test_c.py
+++ generator/test_c.py
@@ -4,7 +4,41 @@ import os
 import ParseCAN
 from common import test_c_dir_path, coord, templ, ifndef, endif, \
     frame_handler, is_multplxd
-from computers_c import msg_handler
+from math import ceil
+
+def single_handler(frame, name_prepends, num_tabs, fw):
+    tot_name = coord(name_prepends, frame.name, prefix=False)
+    fw(
+        '\t' * num_tabs + 'case CANlib_{}_key:'.format(tot_name) + '\n' +
+        '\t' * (num_tabs + 1) + 'CANlib_Handle_{}(&frame);\n'.format(tot_name) +
+        '\t' * (num_tabs + 1) + 'break;\n'
+    )
+
+
+def multplxd_handler(frame, name_prepends, num_tabs, fw):
+    fw('\t' * num_tabs + 'case {}_key:\n'.format(coord(name_prepends, frame.name)))
+    key_size = ceil(frame.slice.length / 8) * 8
+    key_name = '_'.join([name_prepends,frame.name, 'key'])
+    fw('\t' * (num_tabs + 1) + 'to_bitstring(frame.data, &bitstring);' '\n')
+    fw(
+        '\t' * (num_tabs + 1) + 'uint{}_t {} = EXTRACT(bitstring, {}, {});\n'.format(key_size, key_name, frame.slice.start, frame.slice.length) + '\t' * (num_tabs + 1) + 'switch(' + key_name + ') {' '\n'
+    )
+
+    name_prepends += '_' + frame.name
+
+    for sub_frame in frame.frame:
+        if is_multplxd(sub_frame):
+            multplxd_handler(sub_frame, name_prepends, num_tabs + 2, fw)
+        else:
+            single_handler(sub_frame, name_prepends, num_tabs + 2, fw)
+    fw('\t' * (num_tabs + 1) + '}\n')
+
+
+def msg_handler(msg, busnm, fw):
+    if is_multplxd(msg):
+        multplxd_handler(msg, busnm, 2, fw)
+    else:
+        single_handler(msg, busnm, 2, fw)
 
 
 def write(can, output_path=test_c_dir_path):
