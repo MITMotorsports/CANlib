1c1ed4bc30769236f9c2c2d6bfeaedeb39743d05
Mingfei <mingfeid@mit.edu>
2020-09-15 20:12:32 -0400

    Add template for enums

--- generator/main.py
+++ generator/main.py
@@ -19,6 +19,36 @@ import send_receive
 import bus
 import drivers_inc
 
+from jinja2 import nodes
+from jinja2.ext import Extension
+from jinja2.exceptions import TemplateRuntimeError
+
+
+# FROM: https://github.com/duelafn/python-jinja2-apci/blob/master/jinja2_apci/error.py
+class RaiseExtension(Extension):
+    # This is our keyword(s):
+    tags = set(['raise'])
+
+    # See also: jinja2.parser.parse_include()
+    def parse(self, parser):
+        # the first token is the token that started the tag. In our case we
+        # only listen to "raise" so this will be a name token with
+        # "raise" as value. We get the line number so that we can give
+        # that line number to the nodes we insert.
+        lineno = next(parser.stream).lineno
+
+        # Extract the message from the template
+        message_node = parser.parse_expression()
+
+        return nodes.CallBlock(
+            self.call_method('_raise', [message_node], lineno=lineno),
+            [], [], [], lineno=lineno
+        )
+
+    def _raise(self, msg, caller):
+        raise TemplateRuntimeError(msg)
+
+
 if __name__ == '__main__':
 
     specpath = sys.argv[1]
@@ -29,14 +59,14 @@ if __name__ == '__main__':
     generator_dir = os.path.dirname(sys.argv[0])
     template_dir = os.path.join(generator_dir, "templates")
     template_loader = jinja2.FileSystemLoader(searchpath=template_dir)
-    template_env = jinja2.Environment(loader=template_loader, keep_trailing_newline=True)
+    template_env = jinja2.Environment(loader=template_loader, keep_trailing_newline=True, extensions=[RaiseExtension])
     template_env.globals["can"] = can
     template_env.globals["system"] = system
 
     constants.write(template_env)
     pack_unpack_c.write(template_env)
     pack_unpack_h.write(template_env)
-    enum_atom.write(can)
+    enum_atom.write(template_env)
     send_receive.write(can)
     structs.write(can)
     bus.write(can, system.computer)
