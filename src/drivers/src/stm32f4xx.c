db9447a7ac16644fec0dbcdc910d6646a6022f20
Mingfei <mingfeid@mit.edu>
2020-10-24 11:05:42 -0400

    Indentation

--- src/drivers/src/stm32f4xx.c
+++ src/drivers/src/stm32f4xx.c
@@ -1,13 +1,13 @@
 #include "static.h"
 
-#include "drivers/inc/stm32f4xx.h"
-#include "stm32f4xx_hal.h"
-#include "bus.h"
-#include "driver.h"
 #include <stdint.h>
 #include <string.h>
+#include "bus.h"
+#include "driver.h"
+#include "drivers/inc/stm32f4xx.h"
+#include "stm32f4xx_hal.h"
 #ifdef USING_LOGGING_CALLBACK
-  #include "log.h"
+#include "log.h"
 #endif
 
 extern CAN_HandleTypeDef hcan1;
@@ -17,18 +17,18 @@ extern CAN_HandleTypeDef hcan3;
 HAL_StatusTypeDef CANlib_TransmitFrame(Frame *frame, CANlib_Bus_T bus) {
   CAN_Raw_Bus_T raw_bus = CANlib_GetRawBus(bus);
   int bus_num;
-  CAN_HandleTypeDef* hcan;
-  switch(raw_bus) {
+  CAN_HandleTypeDef *hcan;
+  switch (raw_bus) {
     case CAN_1:
-      hcan = &hcan1;
+      hcan    = &hcan1;
       bus_num = 1;
       break;
     case CAN_2:
-      hcan = &hcan2;
+      hcan    = &hcan2;
       bus_num = 2;
       break;
     case CAN_3:
-      hcan = &hcan3;
+      hcan    = &hcan3;
       bus_num = 3;
       break;
     default:
@@ -38,11 +38,11 @@ HAL_StatusTypeDef CANlib_TransmitFrame(Frame *frame, CANlib_Bus_T bus) {
   CAN_TxHeaderTypeDef pHeader;
   uint32_t pTxMailbox = 0;
 
-  pHeader.DLC = frame->dlc;
-  pHeader.StdId = frame->id;
-  pHeader.IDE = frame->extended ? CAN_ID_EXT: CAN_ID_STD;
-  pHeader.RTR = CAN_RTR_DATA; // Data frame (as opposed to a remote frame)
-  pHeader.TransmitGlobalTime = DISABLE; // Don't replace last 2 bytes of data with TX time.
+  pHeader.DLC                = frame->dlc;
+  pHeader.StdId              = frame->id;
+  pHeader.IDE                = frame->extended ? CAN_ID_EXT : CAN_ID_STD;
+  pHeader.RTR                = CAN_RTR_DATA;  // Data frame (as opposed to a remote frame)
+  pHeader.TransmitGlobalTime = DISABLE;       // Don't replace last 2 bytes of data with TX time.
 #ifdef USING_LOGGING_CALLBACK
   log_frame(frame, bus_num);
 #else
@@ -54,7 +54,7 @@ HAL_StatusTypeDef CANlib_TransmitFrame(Frame *frame, CANlib_Bus_T bus) {
 void CANlib_ReadFrame(Frame *frame, CANlib_Bus_T bus) {
   CAN_Raw_Bus_T raw_bus = CANlib_GetRawBus(bus);
   CAN_HandleTypeDef *hcan;
-  switch(raw_bus) {
+  switch (raw_bus) {
     case CAN_1:
       hcan = &hcan1;
       break;
@@ -70,16 +70,16 @@ void CANlib_ReadFrame(Frame *frame, CANlib_Bus_T bus) {
 
   uint8_t data[8] = {};
   CAN_RxHeaderTypeDef pHeader;
-  for (int fifo = 0; fifo < 2; fifo++) { // There are 2 receive FIFOs
-      if (HAL_CAN_GetRxFifoFillLevel(hcan, fifo) > 0) {
-        HAL_CAN_GetRxMessage(hcan, fifo, &pHeader, data);
-        frame->id = pHeader.IDE == CAN_ID_STD ? pHeader.StdId : pHeader.ExtId;
-        frame->dlc = pHeader.DLC;
+  for (int fifo = 0; fifo < 2; fifo++) {  // There are 2 receive FIFOs
+    if (HAL_CAN_GetRxFifoFillLevel(hcan, fifo) > 0) {
+      HAL_CAN_GetRxMessage(hcan, fifo, &pHeader, data);
+      frame->id  = pHeader.IDE == CAN_ID_STD ? pHeader.StdId : pHeader.ExtId;
+      frame->dlc = pHeader.DLC;
 
-        memcpy(frame->data, data, sizeof(data));
-        frame->extended = pHeader.IDE == CAN_ID_EXT;
-        return;
-      }
+      memcpy(frame->data, data, sizeof(data));
+      frame->extended = pHeader.IDE == CAN_ID_EXT;
+      return;
+    }
   }
 }
 
